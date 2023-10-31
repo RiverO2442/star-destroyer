2ebd0cc9296b3b8d9ef5716394bb9ea8a7c7ba74
longtpt <long.tpt@enotion.io>
2023-10-31 22:54:18 +0700

    🐊

--- todo/web/src/hooks/dataFetcher.ts
+++ todo/web/src/hooks/dataFetcher.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
 
-const url: string = process.env.TODO_API_URL;
+console.log(
+    import.meta.env.TODO_API_URL
+)
+const url: string = process.env.TODO_API_URL ? process.env.TODO_API_URL : "http://localhost:8080";
 export const todoApi = axios.create({baseURL: `${url}/api/`})
