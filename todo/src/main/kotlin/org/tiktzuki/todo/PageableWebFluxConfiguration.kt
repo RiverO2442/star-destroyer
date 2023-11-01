c35c77348b2e341ca0edbb4dfcd48ce8ec91dfba
longtpt <longtpt@unicloud.com.vn>
2023-11-01 23:21:17 +0700

    [🦄] fix pageable param kotlin

--- todo/src/main/kotlin/org/tiktzuki/todo/PageableWebFluxConfiguration.kt
+++ todo/src/main/kotlin/org/tiktzuki/todo/PageableWebFluxConfiguration.kt
@@ -1,16 +1,13 @@
-package org.tiktzuki.todo;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
+package org.tiktzuki.todo
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
 
 @Configuration
-public class PageableWebFluxConfiguration implements WebFluxConfigurer {
-
-    @Override
-    public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
-        configurer.addCustomResolver(new ReactivePageableHandlerMethodArgumentResolver());
+class PageableWebFluxConfiguration : WebFluxConfigurer {
+    override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
+        configurer.addCustomResolver(ReactivePageableHandlerMethodArgumentResolver())
     }
-
 }
